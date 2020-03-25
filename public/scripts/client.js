d9f86b60640c3d0d83d4a2bf5dfd7ff1731e53c6
Tomas Wen Liang <tomas.wenn@gmail.com>
2020-03-25 22:25:57 +0000

    Added functions that loads tweets from database, and creates POST request to store new tweet in database

--- public/scripts/client.js
+++ public/scripts/client.js
@@ -4,3 +4,75 @@
  * Reminder: Use (and do all your DOM work in) jQuery's document ready function
  */
 
+//Prevents XSS using escape method
+const escape = str => {
+  let div = document.createElement("div");
+  div.appendChild(document.createTextNode(str));
+  return div.innerHTML;
+};
+
+const createTweetElement = tweetObject => {
+  const tweetedDate = new Date(tweetObject["created_at"]);
+  const todayDate = new Date();
+  const daysSince = Math.round(
+    (todayDate.getTime() - tweetedDate.getTime()) / 86400000
+  );
+  const markup = `
+    <article class="tweet">
+      <header>
+        <img
+          alt="Profile Avatar"
+          class="tweet-avatar"
+          src="${tweetObject.user.avatars}"
+        />
+        <label class="tweet-username"> ${tweetObject.user.name} </label>
+        <label class="tweet-handle">${tweetObject.user.handle}</label>
+      </header>
+      <p class = "tweet-content"> ${escape(tweetObject.content.text)}</p>
+      <footer>
+        <label class="days-ago">${daysSince} days ago</label>
+        <label class="tweet-icons">⚐ ? ♡ </label>
+      </footer>
+    </article>
+  `;
+
+  return markup;
+};
+
+//render the markup created through createTweetElement
+const renderTweets = tweets => {
+  const markupArr = [];
+
+  for (const tweetObject of tweets) {
+    const tweetElement = createTweetElement(tweetObject);
+    markupArr.push(tweetElement);
+  }
+  markupArr.reverse();
+  $("#tweets-container").html(markupArr.join(" "));
+};
+
+//Ready the document
+$(() => {
+  const loadTweets = () => {
+    $.get("/tweets", data => {
+      renderTweets(data);
+    });
+  };
+  loadTweets(); //<----- load tweets at the beginning
+  $("form").submit(event => {
+    event.preventDefault();
+    $tweetText = $("#tweet-text");
+    if (!$tweetText.val()) {
+      alert("Empty tweet");
+    } else if ($tweetText.val().length > 140) {
+      alert("Message too long");
+    } else {
+      const serializedTweet = $tweetText.serialize();
+      $.post("/tweets/", serializedTweet, () => {
+        //WHEN A POST IS MADE IT WILL EMPTY THE TEXT BOX AND LOAD TWEETS AGAIN
+        $("#tweet-text").val("");
+        loadTweets();
+      });
+    }
+  });
+});
